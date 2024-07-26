35ccac66036b8a43ff2adf7e19b2ff6fa447191c
Ajay Divakaran <ajay_886@hotmail.com>
2024-07-26 23:35:54 +0100

    Install homebrew

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# instal homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 ./install_brew_packages.sh
 ./use_zshrc_template.sh 
