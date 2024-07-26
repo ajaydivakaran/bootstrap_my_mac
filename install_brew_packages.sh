b265a867dd708b6d61db926c8c27b269d5ced63b
Ajay Divakaran <ajay_886@hotmail.com>
2024-07-26 22:48:40 +0100

    Install brew packages

---
+++ install_brew_packages.sh
@@ -0,0 +1,48 @@
+#!/bin/zsh
+
+# List of cask packages to install
+cask_packages=(
+    "brave-browser"
+    "google-chrome"
+    "visual-studio-code"
+    "1password"
+    "flycut"
+    "rectangle"
+    "zoom"
+    "notion"
+)
+
+# Update Homebrew to the latest version
+brew update
+
+# Install each cask package
+for package in $cask_packages; do
+    brew install --cask $package
+    echo "$package has been installed."
+done
+
+echo "All specified cask packages have been installed."
+
+# List of brew packages to install
+brew_packages=(
+    "asdf"
+    "colima"
+    "direnv"
+    "bat"
+    "docker"
+    "docker-completion"
+    "docker-compose"
+    "git"
+    "git-gui"
+    "zsh-autosuggestions"
+    "zsh-history-substring-search"
+    "zsh-syntax-highlighting"
+)
+
+# Install each brew package
+for package in $brew_packages; do
+    brew install $package
+    echo "$package has been installed."
+done
+
+echo "All specified brew packages have been installed."
